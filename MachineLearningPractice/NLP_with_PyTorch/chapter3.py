3950b8889f80e3fc841001c74f8b16cbb435f19e
Enes Grahovac <enesxgrahovac@gmail.com>
2020-11-25 11:57:44 -0500

    ML for Yelp reviews

--- MachineLearningPractice/NLP_with_PyTorch/chapter3.py
+++ MachineLearningPractice/NLP_with_PyTorch/chapter3.py
@@ -186,36 +186,200 @@ print(get_toy_data(50))
 # Instatiating the Adam Optimizer
 import torch.optim as optim
 
-input_dim = 2
-lr = 0.001
+def deep_learning_perceptron():
+    input_dim = 2
+    lr = 0.001
 
-perceptron = Perceptron(input_dim=input_dim)
-bce_loss = nn.BCELoss()
-optimizer = optim.Adam(params=perceptron.parameters(), lr=lr)
+    perceptron = Perceptron(input_dim=input_dim)
+    bce_loss = nn.BCELoss()
+    optimizer = optim.Adam(params=perceptron.parameters(), lr=lr)
 
-batch_size = 1000
-n_epochs = 12
-n_batches = 5
+    batch_size = 1000
+    n_epochs = 12
+    n_batches = 5
 
-# each epoch is a complete pass over the training data
-for epoch_i in range(n_epochs):
-    # the inner loop is over the batches in the dataset
-    for batch_i in range(n_batches):
+    # each epoch is a complete pass over the training data
+    for epoch_i in range(n_epochs):
+        # the inner loop is over the batches in the dataset
+        for batch_i in range(n_batches):
 
-        #step 0: Get the data
-        x_data, y_target = get_toy_data(batch_size)
+            #step 0: Get the data
+            x_data, y_target = get_toy_data(batch_size)
 
-        #step 1: Clear the gradients
-        perceptron.zero_grad()
+            #step 1: Clear the gradients
+            perceptron.zero_grad()
 
-        #step 2: Compute the forward pass of the model
-        y_pred = perceptron(x_data,apply_sigmoid=True)
+            #step 2: Compute the forward pass of the model
+            y_pred = perceptron(
+                x_data,
+                # apply_sigmoid=True
+                )
 
-        #step 3: Compute the loss value that we wish to optimize
-        loss = bce_loss(y_pred,y_target)
+            #step 3: Compute the loss value that we wish to optimize
+            loss = bce_loss(y_pred,y_target)
 
-        #step 4: Propogate the loss signal backward
-        loss.backward()
+            #step 4: Propogate the loss signal backward
+            loss.backward()
 
-        #step 5: Trigger the optimizer to perform one update
-        optimizer.step()
+            #step 5: Trigger the optimizer to perform one update
+            optimizer.step()
+
+'''
+The following is a machine learning example of guessing a restaurants rating
+based on reviews alone. So, is it a good or badly rated restaurant solely
+based on what people reviewed it to be?
+'''
+
+import collections
+from argparse import Namespace
+import pandas as pd
+import re
+
+# Load Data
+args = Namespace(
+    raw_train_dataset_csv="yelp/raw_train.csv",
+    raw_test_dataset_csv="yelp/raw_test.csv",
+    train_proportion=0.7,
+    val_proportion=0.15,
+    test_proportion=0.15,
+    output_munged_csv="yelp/reviews_with_splits_full.csv",
+    seed=1337
+)
+train_reviews = pd.read_csv(args.raw_train_dataset_csv, header=None, names=['rating', 'review'])
+train_reviews = train_reviews[~pd.isnull(train_reviews.review)]
+test_reviews = pd.read_csv(args.raw_test_dataset_csv, header=None, names=['rating', 'review'])
+test_reviews = test_reviews[~pd.isnull(test_reviews.review)]
+
+# Step one is to divide the data into training, validation, and testing splits
+by_rating = collections.defaultdict(list)
+for _, row in train_reviews.iterrows():
+    by_rating[row.rating].append(row.to_dict())
+
+final_list = []
+np.random.seed(args.seed)
+
+for _, item_list in sorted(by_rating.items()):
+    np.random.shuffle(item_list)
+
+    n_total = len(item_list)
+    n_train = int(args.train_proportion * n_total)
+    n_val = int(args.val_proportion * n_total)
+    n_test = int(args.test_proportion * n_total)
+
+    # Give data point a split attribute
+    for item in item_list[:n_train]:
+        item['split'] = 'train'
+
+    for item in item_list[n_train:n_train+n_val]:
+        item['split'] = 'val'
+    
+    for item in item_list[n_train+n_val:n_train+n_val+n_test]:
+        item['split'] = 'test'
+    
+    # Add to final list
+    final_list.extend(item_list)
+
+final_reviews = pd.DataFrame(final_list)
+
+### Minimally cleaning the data
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r"([.,!?])",r" \1", text)
+    text = re.sub(r"[^a-zA-Z.,!?]+",r" ", text)
+    return text
+
+final_reviews.review = final_reviews.review.apply(preprocess_text)
+
+print(final_reviews.head())
+
+from torch.utils.data import Dataset
+
+### A PyTorch Dataset class for the Yelp Review Dataset
+
+class ReviewDataset(Dataset):
+    def __init__(self,review_df,vectorizer):
+        """
+        Args:
+            review_df (pandas.DataFrame): the dataset
+            vectorizer (ReviewVectorizer): vectorizer instantiated from dataset
+        """
+        self.review_df = review_df
+        self._vectorizer = vectorizer
+
+        self.train_df = self.review_df[self.review_df.split=='train']
+        self.train_size = len(self.train_df)
+
+        self.val_df = self.review_df[self.review_df.split=='val']
+        self.val_size = len(self.val_df)
+
+        self.test_df = self.review_df[self.review_df.split=='test']
+        self.test_size = len(self.test_df)
+
+        self._lookup_dict = {
+            'train': (self.train_df,self.train_size),
+            'val': (self.val_df,self.val_size),
+            'test': (self.test_df,self.test_size)
+        }
+
+        self.set_split('train')
+
+    @classmethod
+    def load_dataset_and_make_vectorizer(cls, review_csv):
+        """
+        Load dataset and make a new vectorizer from scratch
+
+        Args:
+            review_csv (str): location of the dataset
+        Returns:
+            an instance of ReviewDataset
+        """
+        review_df = pd.read_csv(review_csv)
+        return cls(review_df,ReviewVectorizer.from_dataframe(review_df))
+
+    def get_vectorizer(self):
+        """ returns the vectorizer """
+        return self._vectorizer
+
+    def set_split(self, split="train")
+        """ selects the splits in the dataset using a solumn in the dataframe
+
+        Args:
+            split (str): one of "train","val", or "test"
+        """
+
+        self._target_split = split
+        self._target_df, self._target_size = self._lookup_dict[split]
+
+    def __len__(self):
+        return self._target_size
+
+    def __getitem__(self,index):
+        """the primary entry point method for PyTorch datasets
+
+        Args:
+            index (int): the index to the data point
+        Returns:
+            a dict of the data point's features (x_data) and label (y_targets)
+        """
+        row = self.__target_df.iloc[index]
+
+        review_vector = \
+            self._vectorizer.vectorize(row.review)
+
+        rating_index = \
+            self._vectorizer.rating_vocab.lookup_token(row.rating)
+        
+        return ('x_data':review_vector,
+                'y_target':rating_index)
+
+    def get_num_batches(self,batch_size):
+        """Given a batch size, return the number of batches in the dataset
+
+        Args:
+            batch_size (int)
+        Returns:
+            number of batches in the dataset
+        """
+        return len(self) // batch_size
+    
