16c1589f7ef4b0414d78ef447355e5f765844372
Enes Grahovac <enesxgrahovac@gmail.com>
2020-12-01 20:37:15 -0500

    Starting PCA practice

---
+++ MachineLearningPractice/internet/PCA.py
@@ -0,0 +1,16 @@
+'''
+Today in a meeting I was asked about Principal Component Analysis (PCA) and realized that it is important 
+that I learn it. PCA is an unsupervised, non-parametric statistical technique primarily used
+for dimensionality reduction in machine learning.
+
+Below are some tutorials and papers I followed from on the internet and practiced how to apply PCA in python.
+'''
+
+
+
+'''
+References:
+
+https://medium.com/apprentice-journal/pca-application-in-machine-learning-4827c07a61db
+
+'''
